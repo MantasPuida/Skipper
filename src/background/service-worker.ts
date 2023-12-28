92bde4cdeb51d44374baa0d77b570d32bb6b80bf
Mantas <mantasxpuida@gmail.com>
2023-12-28 16:29:36 +0200

    development

--- src/background/service-worker.ts
+++ src/background/service-worker.ts
@@ -1,6 +1,8 @@
 import { ServiceWorkerConstants } from "../utils/service-worker-constants";
 
 class BackgroundServiceWorker {
+	private validateElement = (element: Element): element is HTMLButtonElement => (element as HTMLButtonElement).click !== undefined;
+
 	private handleInjection = (frameId: number, tabId: number, parentSelector: string, classNames: string[]) => {
 		const handleScriptInjection = (...args: [string, string[]]) => {
 			const [parentSelectorArg, classNamesArg] = args;
@@ -8,13 +10,18 @@ class BackgroundServiceWorker {
 			const chain = parentSelectorNode ?? document;
 
 			classNamesArg.forEach(className => {
-				const elementsWithClassName = chain.getElementsByClassName(className) as HTMLCollectionOf<HTMLButtonElement>;
+				const elementsWithClassName = chain.getElementsByClassName(className);
 				if (elementsWithClassName.length === 0) {
 					return;
 				}
 
 				for (let i = 0; i < elementsWithClassName.length; i++) {
 					const buttonElement = elementsWithClassName[i];
+					const isValidButtonElement = this.validateElement(buttonElement);
+					if (!isValidButtonElement) {
+						return;
+					}
+
 					buttonElement.click();
 				}
 			});
@@ -29,17 +36,17 @@ class BackgroundServiceWorker {
 	};
 
 	private handleAdSkip = (...args: [number, number]) => {
-		this.handleInjection(...args, ServiceWorkerConstants.SkipButtonParentSelector, ServiceWorkerConstants.SkipButtonSelectors);
+		const { SkipButtonParentSelector, SkipButtonSelectors } = ServiceWorkerConstants;
+		this.handleInjection(...args, SkipButtonParentSelector, SkipButtonSelectors);
 	};
 
 	private handleCompleted = (details: chrome.webRequest.WebRequestBodyDetails) => {
 		const { url, frameId, tabId } = details;
-		const myUrl = new URL(url);
-		const { searchParams } = myUrl;
+		const { searchParams } = new URL(url);
 
 		const label = searchParams.get(ServiceWorkerConstants.RequestParamKey);
 		switch (label) {
-			case ServiceWorkerConstants.RequestLabels.SkipShown:
+			case ServiceWorkerConstants.RequestLabels.AdUnit:
 				this.handleAdSkip(frameId, tabId);
 				break;
 			default:
--- src/utils/service-worker-constants.ts
+++ src/utils/service-worker-constants.ts
@@ -1,14 +1,16 @@
 export namespace ServiceWorkerConstants {
-	export const WebRequestUrls = ["https://www.youtube.com/pagead/interaction/*"];
+	export const WebRequestUrls = ["https://www.youtube.com/api/stats/atr*"];
 	export const WebRequestTypes: chrome.webRequest.ResourceType[] = ["xmlhttprequest"];
 
 	export const SkipButtonSelectors = ["ytp-ad-skip-button ytp-button", "ytp-ad-skip-button-modern ytp-button"];
 	export const SkipButtonParentSelector = "ytd-player";
 
-	export const RequestParamKey = "label";
+	export const RequestParamKey = "el";
 	export enum RequestLabels {
+		AdUnit = "adunit",
 		SkipShown = "video_skip_shown",
 		Skipped = "videoskipped",
-		ImpressionTracking = "video_companion_impression_tracking"
+		ImpressionTracking = "video_companion_impression_tracking",
+		FullPlaytime = "videoplaytime100"
 	}
 }
